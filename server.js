8235610c8e46fb05da653be2bb043cb07fe05b6e
NishanthAnand <nishanth0228@gmail.com>
2017-10-06 13:37:56 -0500

    Update server.js

--- server.js
+++ server.js
@@ -21,8 +21,54 @@ var open = require('open');
 //app.use(express.static(__dirname + '/public'));
 
 
-var connectionString = "mongodb://localhost/test";
-	//var connectionString = "mongodb://admin:gRvhq_LLpEWI@56f43bc989f5cfb1aa000128-trialuta.rhcloud.com:48111/";
+
+
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
+    mongoURLLabel = "";
+
+if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
+    var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
+        mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
+        mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
+        mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
+        mongoPassword = process.env[mongoServiceName + '_PASSWORD']
+    mongoUser = process.env[mongoServiceName + '_USER'];
+
+    if (mongoHost && mongoPort && mongoDatabase) {
+        mongoURLLabel = mongoURL = 'mongodb://';
+        if (mongoUser && mongoPassword) {
+            mongoURL += mongoUser + ':' + mongoPassword + '@';
+        }
+        // Provide UI label that excludes user id and pw
+        mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
+        mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
+
+    }
+}
+
+var initDb = function() {
+    if (mongoURL == null) return;
+
+
+    mongoose.connect(mongoURL, function(err, conn) {
+        if (err) {
+            callback(err);
+            return;
+        }
+
+        db = conn;
+        dbDetails.databaseName = db.databaseName;
+        dbDetails.url = mongoURLLabel;
+        dbDetails.type = 'MongoDB';
+
+        console.log('Connected to MongoDB at: %s', mongoURL);
+    });
+};
+
+//var connectionString = "mongodb://localhost/test";
+var connectionString = mongoURL;
 
 var app = express();
 app.use(express.static(__dirname + '/public'));                // set the static files location /public/img will be /img for users
@@ -33,7 +79,8 @@ app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse applica
 app.use(methodOverride());
 
 
-mongoose.connect(connectionString); //connects to local DB
+initDb();
+//mongoose.connect(connectionString); //connects to local DB
 /* var ArticleSchema = new mongoose.Schema({
 	title : String,
 	iframeLink : String,
